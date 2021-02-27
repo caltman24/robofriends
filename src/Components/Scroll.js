b75bc8048749559e78ceaa29b2285f0d7a0f48ef
Corbyn <corbyn.altman01@gmail.com>
2021-02-26 23:47:51 -0500

    changed scroll height

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div className="ma2 br3"style={{overflowY: 'scroll', border: '5px solid rgba(229, 255, 0, 0.3)', height: '500px'}}>
+        <div className="ma2 br3"style={{overflowY: 'scroll', border: '5px solid rgba(229, 255, 0, 0.3)', height: '75vh'}}>
             {props.children}
         </div>
     )
